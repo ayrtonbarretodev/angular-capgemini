e145f7cab4de0808a82fa02932d8b006ab28043c
Ayrton <devayrtonsbarreto@gmail.com>
2022-10-09 22:59:25 -0300

    Finalizando aplicação

--- src/app/produtos/produtos.component.ts
+++ src/app/produtos/produtos.component.ts
@@ -1,3 +1,4 @@
+import { ActivatedRoute } from '@angular/router';
 import { IProduto, produtos } from './../produtos';
 import { Component, OnInit } from '@angular/core';
 import { ProdutosService } from '../produtos.service';
@@ -11,10 +12,21 @@ export class ProdutosComponent implements OnInit {
   produtos: IProduto[] | undefined;
 
 
-  constructor(private produtosService: ProdutosService) { }
+  constructor(private produtosService: ProdutosService, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.produtos = this.produtosService.getAll();
+    const produtos = this.produtosService.getAll();
+
+    this.route.queryParamMap.subscribe(params => {
+      const descricao = params.get("descricao")?.toLowerCase();
+
+      if(descricao){
+        this.produtos = produtos.filter(produto => produto.descricao.toLowerCase().includes(descricao));
+        return;
+      }
+
+      this.produtos = produtos;
+    });
   }
 
 }
